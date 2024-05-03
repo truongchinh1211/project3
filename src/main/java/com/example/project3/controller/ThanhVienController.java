5d2be9b030384940e7d391fa44e9d0a97afb011b
truongchinh1211 <nntchinh2001@gmail.com>
2024-05-03 08:52:29 +0700

    -n

--- src/main/java/com/example/project3/controller/ThanhVienController.java
+++ src/main/java/com/example/project3/controller/ThanhVienController.java
@@ -3,10 +3,12 @@ package com.example.project3.controller;
 import com.example.project3.entity.ThanhVien;
 import com.example.project3.service.ThanhVienService;
 import com.example.project3.service.JwtService;
+import com.nimbusds.jose.JOSEException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -20,10 +22,15 @@ public class ThanhVienController {
     JwtService jwtService;
 
     @PostMapping("/login")
-    public ResponseEntity<ThanhVien> checkLogin(@RequestBody Map<String,String> loginRequest){
+    public ResponseEntity<Map<String,Object>> checkLogin(@RequestBody Map<String,String> loginRequest) throws JOSEException {
         String email = loginRequest.get("email");
         String password = loginRequest.get("password");
-        return ResponseEntity.ok(thanhVienService.login(email,password));
+        ThanhVien thanhVien = thanhVienService.login(email,password);
+        String token = jwtService.generateToken(email);
+        Map<String,Object> response = new HashMap<>();
+        response.put("thanhVien",thanhVien);
+        response.put("token",token);
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping()

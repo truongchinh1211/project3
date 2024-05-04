82224272a3ba70bb79c817631483cb7145a9c674
truongchinh1211 <nntchinh2001@gmail.com>
2024-05-04 16:55:20 +0700

    Update ThongTinSdService.java

--- src/main/java/com/example/project3/service/ThongTinSdService.java
+++ src/main/java/com/example/project3/service/ThongTinSdService.java
@@ -2,12 +2,14 @@ package com.example.project3.service;
 
 import com.example.project3.dto.ThongTinSdDTO;
 import com.example.project3.entity.ThongTinSd;
+import com.example.project3.exception.ResourceNotFoundException;
 import com.example.project3.repository.ThongTinSDRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class ThongTinSdService {
@@ -15,15 +17,21 @@ public class ThongTinSdService {
     private ThongTinSDRepository thongTinSDRepository;
 
     @Transactional
-    public ThongTinSd datCho(ThongTinSdDTO thongTinSdDTO) throws Exception {
+    public ThongTinSdDTO datCho(ThongTinSdDTO thongTinSdDTO) throws Exception {
         ThongTinSd thongTinSd = convertToEntity(thongTinSdDTO);
         if(thongTinSDRepository.findByTGDatCho(thongTinSd.getTGDatCho().toLocalDate()).isPresent()){
             throw new Exception("Lỗi");
         }
-        return thongTinSDRepository.save(thongTinSd);
+        return convertToDTO(thongTinSDRepository.save(thongTinSd));
     }
 
-
+    @Transactional
+    public ThongTinSdDTO muon(ThongTinSdDTO thongTinSdDTO) throws Exception {
+        ThongTinSd existedThongTinSd = thongTinSDRepository.findById(thongTinSdDTO.getMaTT())
+                .orElseThrow(() -> new ResourceNotFoundException("không tìm thấy thông tin có mã: "+thongTinSdDTO.getMaTT()));
+        existedThongTinSd.setTGMuon(thongTinSdDTO.getTGMuon());
+        return convertToDTO(thongTinSDRepository.save(existedThongTinSd));
+    }
 
 
     public ThongTinSd convertToEntity(ThongTinSdDTO thongTinSdDTO){

b483c8886f3f71b18dc7168e55f41d31a06f5138
TryMyBest-MinhHieu <dominhhieu6119@gmail.com>
2024-05-15 20:46:35 +0700

    fix Login

--- src/main/resources/Client/User/login.js
+++ src/main/resources/Client/User/login.js
@@ -1,34 +1,27 @@
-function postData(url, data, callback) {
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", url, true);
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      var response = JSON.parse(xhr.responseText);
-      callback(response);
-    } else if (xhr.readyState === 4 && xhr.status === 400) {
-      console.log("Sai email hoặc mật khẩu");
-    }
-  };
-  var jsonData = JSON.stringify(data);
-  xhr.send(jsonData);
-}
+document.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-document.addEventListener("DOMContentLoaded", function () {
-  document.addEventListener("submit", function (event) {
-    event.preventDefault();
-    var apiUrl = "http://localhost:8080/api/v1/user/login";
-    var dataToSend = {
-      email: document.getElementById("username").value,
-      password: document.getElementById("password").value,
-    };
-    postData(apiUrl, dataToSend, function (response) {
-      console.log("Object:", response);
-      var token = response.token;
-      localStorage.setItem("token", token);
-      console.log("Token in local storage:", localStorage.getItem("token"));
-      console.log("Catch token:", token);
-      window.location.href = "../homepage/datcho.html";
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  fetch("http://localhost:8080/api/v1/user/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username: username, password: password }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.success) {
+        window.location.href = "home.html";
+      } else {
+        alert("Tài khoản hoặc mật khẩu bạn sai");
+        document.getElementById("password").value = "";
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      alert("Có lỗi xảy ra, vui lòng thử lại sau.");
     });
-  });
 });
